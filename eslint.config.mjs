afaf6b5342bc973a152842b7a09be26c1ea96b8d
Tanawat Hongthai <xiivth.dev@gmail.com>
2026-05-27 22:59:29 +0700

    chore: ignore generated build outputs in lint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,19 @@
 import nextVitals from "eslint-config-next/core-web-vitals";
 import nextTs from "eslint-config-next/typescript";
 
-const config = [...nextVitals, ...nextTs];
+const config = [
+  {
+    ignores: [
+      ".next/**",
+      "storybook-static/**",
+      ".storybook-static/**",
+      "node_modules/**",
+      "coverage/**",
+      "next-env.d.ts",
+    ],
+  },
+  ...nextVitals,
+  ...nextTs,
+];
 
 export default config;
