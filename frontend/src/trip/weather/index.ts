d6dfa97cb2ef5cd807bb748fbcf6b290d4ccf001
Tanawat Hongthai <xiivth.dev@gmail.com>
2026-06-22 00:16:53 +0700

    refactor(frontend): make weather exports explicit

--- frontend/src/trip/weather/index.ts
+++ frontend/src/trip/weather/index.ts
@@ -1,3 +1,22 @@
-export * from "./weather-briefing-types";
-export * from "./weather-briefing-display";
-export * from "./weather-briefings";
+export type {
+  BriefingCoordinates,
+  BriefingSourceMeta,
+  DailyBriefingOverrides,
+  TextBriefingBlock,
+  TripDailyBriefing,
+  WeatherBriefingBlock,
+} from "./weather-briefing-types";
+export {
+  briefingsForStrip,
+  formatSolarTime,
+  formatWeatherTemp,
+  thaiWeekdayTone,
+  weatherGraphicLabel,
+  weatherIconForCondition,
+} from "./weather-briefing-display";
+export type { ThaiWeekdayTone } from "./weather-briefing-display";
+export {
+  applyDailyBriefingOverrides,
+  buildFallbackBriefings,
+  buildPatchDailyBriefingRequest,
+} from "./weather-briefings";
